25e1b0f8694922d6b08ec96e177a1337f377b270
masrafi-000 <smmasrafi000@gmail.com>
2024-09-17 17:26:22 +0600

    Updated

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '45745',
+        paymentStatus: 'Paid',
+        shipping: 'Deliverd'
+    },
+    {
+        productName: 'GFS 4k Drone',
+        productNumber: '55744',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '45878',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '00441',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
